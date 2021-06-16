c3b2b01e1fa5bf81b62b9f02aab16cf28e5614a1
boomer <boomersplayground@gmail.com>
2021-06-16 10:56:31 -0700

    pushing up with super user created and Posts creation on Admin dashboard

--- blogSite/settings.py
+++ blogSite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'blogSite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -105,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+# America/Phoenix
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
